486a656908631274f7f7f499a2e4550d0e0c3ca0
ellen-rose-james <ellenrj03@gmail.com>
2024-08-31 14:31:48 +0530

    done

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
+console.log("I'm ready")
 // Iteration 1: Names and Input
-//
+hacker1 = "James"
+console.log(`The driver's name is ${hacker1}`)
+
+hacker2 = "John"
+console.log(`The navigtors's name is ${hacker2}`)
 // Iteration 2: Conditionals
 
 
